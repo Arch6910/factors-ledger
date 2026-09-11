fc8fb76bd877da1238942daecf8530f7b2cd1754
Arch6910 <Shahrukh081899@gmail.com>
2026-09-12 00:07:03 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+var CACHE_NAME = "factors-ledger-v2";
+var APP_SHELL = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-512-maskable.png",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (key) { return key !== CACHE_NAME; }).map(function (key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      var fetchPromise = fetch(event.request)
+        .then(function (networkResponse) {
+          if (networkResponse && networkResponse.ok) {
+            var copy = networkResponse.clone();
+            caches.open(CACHE_NAME).then(function (cache) {
+              cache.put(event.request, copy);
+            });
+          }
+          return networkResponse;
+        })
+        .catch(function () {
+          return cached;
+        });
+      return cached || fetchPromise;
+    })
+  );
+});
